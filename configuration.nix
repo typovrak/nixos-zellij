98cd534036d868d6385b15db04009afe5bd4685d
typovrak <typovrak@gmail.com>
2025-07-06 21:46:01 +0200

    add zellij with catppuccin mocha theme

---
+++ configuration.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+let 
+	username = config.username;
+	group = config.users.users.${username}.group or "users";
+	home = config.users.users.${username}.home;
+in {
+	system.activationScripts.zellij = ''
+		mkdir -p ${home}/.config
+		chown ${username}:${group} ${home}/.config
+		chmod 700 ${home}/.config
+
+		rm -rf ${home}/.config/zellij
+		mkdir ${home}/.config/zellij
+		chown ${username}:${group} ${home}/.config/zellij
+		chmod 700 ${home}/.config/zellij
+
+		cp ${./zellij.conf} ${home}/.config/zellij/config.kdl
+		chown ${username}:${group} ${home}/.config/zellij/config.kdl
+		chmod 600 ${home}/.config/zellij/config.kdl
+	'';
+
+	environment.systemPackages = with pkgs; [
+		zellij
+	];
+}
